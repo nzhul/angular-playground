9a2b790dd8b4fb8d2226f964ec9112dad2cf9c32
Dobromir Ivanov <dobromirivanov1@gmail.com>
2018-03-19 18:12:28 +0200

    adding client login and registration

--- DatingApp/DatingApp.SPA/src/app/app.module.ts
+++ DatingApp/DatingApp.SPA/src/app/app.module.ts
@@ -4,17 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ValueComponent,
     NavComponent
+,
+    HomeComponent,
+    RegisterComponent
 ],
   imports: [
       BrowserModule,
