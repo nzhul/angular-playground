7abfe7b6305aef78fc004b8a265c263d658befdc
nzhul <dobromirivanov1@gmail.com>
2018-03-25 20:19:42 +0300

    starting work on user details component

---
+++ DatingApp/DatingApp.SPA/src/app/_services/user.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Http, RequestOptions, Headers } from '@angular/http';
+import { User } from '../_models/User';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import { AuthHttp } from 'angular2-jwt';
+
+@Injectable()
+export class UserService {
+
+    baseUrl = environment.apiUrl;
+
+    constructor(private authHttp: AuthHttp) { }
+
+    getUsers(): Observable<User[]> {
+        // use this when not using authHttp -> return this.authHttp.get(this.baseUrl + 'users', this.jwt())
+        return this.authHttp.get(this.baseUrl + 'users')
+        .map(response => <User[]>response.json())
+        .catch(this.handleError);
+    }
+
+    getUser(id): Observable<User> {
+        return this.authHttp
+        .get(this.baseUrl + 'users/' + id)
+        .map(response => <User>response.json())
+        .catch(this.handleError);
+    }
+
+    // this is no longer used but i will leave it for reference
+    // we are now using AuthModule to send our json web token with every request.
+    private jwt() {
+        const token = localStorage.getItem('token');
+        if (token) {
+            const headers = new Headers({ 'Authorization': 'Bearer ' + token });
+            headers.append('Content-type', 'application/json');
+
+            return new RequestOptions({ headers: headers });
+        }
+    }
+
+    private handleError(error: any) {
+        const applicationError = error.headers.get('Application-Error');
+        if (applicationError) {
+            return Observable.throw(applicationError);
+        }
+        const serverError = error.json();
+        let modelStateErrors = '';
+        if (serverError) {
+            for (const key in serverError) {
+                if (serverError[key]) {
+                    modelStateErrors += serverError[key] + '\n';
+                }
+            }
+        }
+        return Observable.throw(modelStateErrors || 'Server error');
+    }
+}
--- DatingApp/DatingApp.SPA/src/app/app.module.ts
+++ DatingApp/DatingApp.SPA/src/app/app.module.ts
@@ -10,39 +10,45 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { AlertifyService } from './_services/alertify.service';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
 import { AuthGuard } from './_guards/auth.guard';
+import { UserService } from './_services/user.service';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { AuthModule } from './auth/auth.module';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent
-,
-    HomeComponent,
-    RegisterComponent
-,
-    MemberListComponent,
-    ListsComponent,
-    MessagesComponent
-],
-  imports: [
-      BrowserModule,
-      HttpModule,
-      FormsModule,
-      BsDropdownModule.forRoot(),
-      RouterModule.forRoot(appRoutes)
-  ],
-  providers: [
-      AuthService,
-      AlertifyService,
-      AuthGuard
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavComponent,
+        HomeComponent,
+        RegisterComponent,
+        MemberListComponent,
+        ListsComponent,
+        MessagesComponent,
+        MemberCardComponent,
+        MemberDetailComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        FormsModule,
+        BsDropdownModule.forRoot(),
+        RouterModule.forRoot(appRoutes),
+        AuthModule
+    ],
+    providers: [
+        AuthService,
+        AlertifyService,
+        AuthGuard,
+        UserService
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
