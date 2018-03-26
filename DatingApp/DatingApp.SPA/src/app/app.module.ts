794a06f3fb01d555245762089e0540d554f1a227
nzhul <dobromirivanov1@gmail.com>
2018-03-27 00:33:45 +0300

    working on profile update page

--- DatingApp/DatingApp.SPA/src/app/app.module.ts
+++ DatingApp/DatingApp.SPA/src/app/app.module.ts
@@ -23,6 +23,8 @@ import { AuthModule } from './auth/auth.module';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 
 
 
@@ -36,7 +38,8 @@ import { MemberListResolver } from './_resolvers/member-list.resolver';
         ListsComponent,
         MessagesComponent,
         MemberCardComponent,
-        MemberDetailComponent
+        MemberDetailComponent,
+        MemberEditComponent
     ],
     imports: [
         BrowserModule,
@@ -54,7 +57,8 @@ import { MemberListResolver } from './_resolvers/member-list.resolver';
         AuthGuard,
         UserService,
         MemberDetailResolver,
-        MemberListResolver
+        MemberListResolver,
+        MemberEditResolver
     ],
     bootstrap: [AppComponent]
 })
