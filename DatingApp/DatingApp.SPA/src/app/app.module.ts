b819a4df12df0fe0554f004cd9c5aa5ef51ff542
nzhul <dobromirivanov1@gmail.com>
2018-03-29 00:40:01 +0300

    adding unsaved changes guard and user update method in the api

--- DatingApp/DatingApp.SPA/src/app/app.module.ts
+++ DatingApp/DatingApp.SPA/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 
 
@@ -58,7 +59,8 @@ import { MemberEditResolver } from './_resolvers/member-edit.resolver';
         UserService,
         MemberDetailResolver,
         MemberListResolver,
-        MemberEditResolver
+        MemberEditResolver,
+        PreventUnsavedChanges
     ],
     bootstrap: [AppComponent]
 })
